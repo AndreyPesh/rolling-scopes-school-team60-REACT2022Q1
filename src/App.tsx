3c744b020ad9335130f90d3d1024381b7647bfb1
umidullo <suyunovumidullo@gmail.com>
2022-05-08 20:36:21 +0500

    feat: configure redux and add one test slice

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
 import './App.scss';
+import { useAppDispatch, useAppSelector } from './hooks';
+import { decrement, increment } from './store/slices/counter';
 
-const App = () => <div className="app">Rolling-scopes-school-team60-REACT2022Q1</div>;
+const App = () => {
+  const dispatch = useAppDispatch();
+  const value = useAppSelector((state) => state.count.value);
+
+  return (
+    <div>
+      <h1>counter</h1>
+      <button onClick={() => dispatch(increment(10))}> + </button>
+      <h2>{value}</h2>
+      <button onClick={() => dispatch(decrement(10))}> - </button>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/store/index.ts
@@ -0,0 +1,13 @@
+import { configureStore } from '@reduxjs/toolkit';
+import countReducer from './slices/counter';
+
+const store = configureStore({
+  reducer: {
+    count: countReducer,
+  },
+});
+
+export default store;
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
