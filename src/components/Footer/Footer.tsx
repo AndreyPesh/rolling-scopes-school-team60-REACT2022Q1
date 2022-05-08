6b806c68e6d7e167d70deb43d54b5dea17c77d29
MielomankA <mielomanka@mail.ru>
2022-05-08 23:34:07 +0300

    feat: add footer layout

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,3 +1,24 @@
-const Footer = () => <footer>Footer</footer>;
+import { team } from '../../utils/constants/constants';
+
+import './Footer.scss';
+import FooterLogo from './footerComponents/FooterLogo';
+import FooterNavItem from './footerComponents/FooterNavItem';
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container footer__container">
+        <FooterLogo />
+        <nav className="team">
+          <ul className="team__list">
+            {team.map((person) => {
+              return <FooterNavItem key={person.name} link={person.github} title={person.name} />;
+            })}
+          </ul>
+        </nav>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
---
+++ src/utils/constants/constants.ts
@@ -0,0 +1,18 @@
+export const team = [
+  {
+    name: 'Mentor: Maryna Rastargueva',
+    github: 'https://github.com/maryna-js',
+  },
+  {
+    name: 'Andrey Peshko',
+    github: 'https://github.com/AndreyPesh',
+  },
+  {
+    name: 'Nataliya Belyaeva',
+    github: 'https://github.com/MielomankA',
+  },
+  {
+    name: 'Umidullo Suyunov',
+    github: 'https://github.com/umidullo',
+  },
+];
