a3b3eafe0cd69c87f0e2acd401bcfb470bf454ea
umidullo <suyunovumidullo@gmail.com>
2022-05-24 00:31:14 +0500

    feat: add custom form

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,18 +1,46 @@
-import { Button, TextField, Grid, Box, Typography, Container } from '@mui/material';
+import { Button, Grid, Box, Typography, Container, TextField } from '@mui/material';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
 import { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import Spinner from '../../components/Spinner/Spinner';
 import { useAppDispatch } from '../../hooks/useAppDispatch';
 import { useAppSelector } from '../../hooks/useAppSelector';
 import { Path } from '../../router/routes';
 import { signIn } from '../../store/slices/authSlice';
+import BasicModal from '../../components/Modal/Modal';
+import ModalForm from '../../components/ModalForm';
+
+const fields = [
+  {
+    name: 'login',
+    type: 'text',
+    validationsRules: {
+      required: 'this input is required',
+      minLength: {
+        value: 8,
+        message: 'minimum 8 characters',
+      },
+    },
+  },
+  {
+    name: 'password',
+    type: 'password',
+    validationsRules: {
+      required: 'this input is required',
+      minLength: {
+        value: 8,
+        message: 'minimum 8 characters',
+      },
+    },
+  },
+];
 
 const Login = () => {
   const navigate = useNavigate();
-  const dispatch = useAppDispatch();
-  const { isLoading, error, token } = useAppSelector((state) => state.auth);
+  const { isLoading, token, error } = useAppSelector((state) => state.auth);
 
-  const [login, setLogin] = useState('');
-  const [password, setPassword] = useState('');
+  const [popup, setPopup] = useState(false);
 
   useEffect(() => {
     if (token) {
@@ -20,59 +48,46 @@ const Login = () => {
     }
   }, [token, navigate]);
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-    const data = {
-      login,
-      password,
-    };
-
-    dispatch(signIn(data));
-
-    setLogin('');
-    setPassword('');
-  };
+  useEffect(() => {
+    if (error) {
+      setPopup(true);
+    }
+  }, [error]);
 
   return (
-    <Container component="main" maxWidth="xs">
-      {isLoading && <Typography>Loading...</Typography>}
-      {error && <Typography>{error}</Typography>}
-      <Typography component="h1" variant="h5">
-        Sign in
-      </Typography>
-      <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          id="login"
-          label="Login "
-          name="login"
-          value={login}
-          onChange={(e) => setLogin(e.target.value)}
+    <>
+      {
+        <BasicModal
+          open={popup}
+          handleClose={() => setPopup(false)}
+          content={
+            <Typography component="h1" variant="h5">
+              {error}
+              <Button
+                onClick={() => setPopup(false)}
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Ok
+              </Button>
+            </Typography>
+          }
         />
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          name="password"
-          label="Password"
-          type="password"
-          id="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
-          Sign In
-        </Button>
-        <Grid container>
-          <Grid item>
-            <Link to={`/${Path.signup}`}>{"Don't have an account? Sign Up"}</Link>
-          </Grid>
-        </Grid>
-      </Box>
-    </Container>
+      }
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <Container component="main" maxWidth="xs">
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box sx={{ mt: 1 }}>
+            <ModalForm inputObj={fields} submitFunc={signIn} submitBtnName="Sign In" />
+          </Box>
+        </Container>
+      )}
+    </>
   );
 };
 
