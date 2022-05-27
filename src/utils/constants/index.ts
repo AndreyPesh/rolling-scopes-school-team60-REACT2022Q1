e8fd444fc2531aeb6a2e714bd75d83b95ce8b5c2
umidullo <suyunovumidullo@gmail.com>
2022-05-27 15:22:44 +0500

    fix: backend url

--- src/utils/constants/index.ts
+++ src/utils/constants/index.ts
@@ -4,7 +4,7 @@ import { Developer } from '../interfaces/interfaces';
 export const RESET_TOKEN = '';
 export const TOKEN = 'token';
 
-export const BASE_URL = 'https://mighty-earth-43476.herokuapp.com';
+export const BASE_URL = 'https://pure-chamber-11952.herokuapp.com';
 export const SIGNIN_URL = '/signin';
 export const SIGNUP_URL = '/signup';
 export const ALL_USERS_URL = '/users';
