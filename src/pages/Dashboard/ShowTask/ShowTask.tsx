fca8453479329b23ac2af728eec1c6c50bff2654
Andrey Peshko <coursestud@mail.ru>
2022-05-27 22:55:19 +0300

    feat: add show task component

---
+++ src/pages/Dashboard/ShowTask/ShowTask.tsx
@@ -0,0 +1,5 @@
+const ShowTask = () => {
+  return <h2>show task</h2>;
+};
+
+export default ShowTask;
--- src/pages/Dashboard/Task/Task.tsx
+++ src/pages/Dashboard/Task/Task.tsx
@@ -1,9 +1,14 @@
+import { MouseEvent } from 'react';
 import { Paper } from '@mui/material';
 import './task.scss';
 import { TaskData } from '../../../utils/types/types';
 import RemoveTask from '../RemoveTask/RemoveTask';
 import { Draggable } from 'react-beautiful-dnd';
 import EditTask from '../EditTask/EditTask';
+import { useAppDispatch } from '../../../hooks';
+import { openModal } from '../../../store/slices/modalSlice';
+import ShowTask from '../ShowTask/ShowTask';
+import { NAME_DATA_ATRR_SHOW_TASK } from '../../../utils/constants';
 
 const Task: React.FC<{ columnId: string; dataTask: TaskData; index: number }> = ({
   columnId,
@@ -11,6 +16,13 @@ const Task: React.FC<{ columnId: string; dataTask: TaskData; index: number }> = 
   index,
 }) => {
   const { title } = dataTask;
+  const dispatch = useAppDispatch();
+  const showTask = (event: MouseEvent<HTMLDivElement>) => {
+    const { target } = event;
+    if ((target as Element).getAttribute(NAME_DATA_ATRR_SHOW_TASK)) {
+      dispatch(openModal({ open: true, contentModal: <ShowTask /> }));
+    }
+  };
   return (
     <Draggable draggableId={dataTask.id} index={index}>
       {(provided) => (
@@ -19,8 +31,10 @@ const Task: React.FC<{ columnId: string; dataTask: TaskData; index: number }> = 
           {...provided.draggableProps}
           {...provided.dragHandleProps}
           ref={provided.innerRef}
+          data-show="task"
+          onClick={showTask}
         >
-          <h2>{title}</h2>
+          <h2 data-show="task">{title}</h2>
           <span>
             <EditTask columnId={columnId} dataTask={dataTask} />
             <RemoveTask columnId={columnId} dataTask={dataTask} />
